5b320e59bb468e9be9db6564b9e7c420142033ca
lewismuithya <lewismuithya@gmail.com>
2026-02-02 08:43:54 +0300

    updated js

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ function generateAkanName(){
     let year = parseInt(document.getElementById("year").value);
     let gender = document.querySelector('input[name="gender"]:checked');
 
-    
     if(!gender || day < 1 || day > 31 || month < 1 || month > 12){
         alert("Please enter a valid date and select gender.");
         return;
     }
 
-   
     let birthDate = new Date(year, month - 1, day);
-    let dayOfWeek = birthDate.getDay();  
+    let dayOfWeek = birthDate.getDay();
 
-    
     let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
     let akanName = gender.value === "male" ? maleNames[dayOfWeek] : femaleNames[dayOfWeek];
 
-    
-    document.getElementById("result").innerText = "Your Akan name is: " + akanName;
+    let resultBox = document.getElementById("result");
+    resultBox.innerText = "Your Akan name is: " + akanName;
+
+    resultBox.style.animation = "none";
+    resultBox.offsetHeight;
+    resultBox.style.animation = "fadeInResult 0.8s ease forwards";
 }
